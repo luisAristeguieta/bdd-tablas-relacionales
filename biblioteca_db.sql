4bed7a0500e653b1dcffb8729199ab1a8975b610
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-05-26 16:19:03 -0400

    feat: crear tablas libros y autores

--- biblioteca_db.sql
+++ biblioteca_db.sql
@@ -1,1 +1,21 @@
 CREATE DATABASE biblioteca_db;
+
+--Parte # 2 Creacion de tablas: (libros y autores)
+
+CREATE TABLE libros (
+    codigo VARCHAR(5) PRIMARY KEY,
+    titulo VARCHAR(50) NOT NULL,
+    paginas INT NOT NULL
+);
+	
+CREATE TABLE autores (
+	id INT PRIMARY KEY,
+	nombre VARCHAR(50) NOT NULL,
+	pais VARCHAR(30) NOT NULL
+);
+
+--Verifica existencia: 
+SELECT * FROM libros;
+SELECT * FROM autores;
+
+
